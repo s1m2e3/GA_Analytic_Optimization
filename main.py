0abd1a4cc6f31b0e311c2767a86ecaf35e95a9fb
kylenorland <norland.kyle@gmail.com>
2022-10-10 11:06:43 -0700

    Added comments and debugging

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#Sam, modified by Kyle Norland
+#Modified 10/10/22
+
 import numpy as np
 import cvxpy as cvx
 import random
@@ -15,42 +18,84 @@ import os
 def create_sols(n_states,n_learners):
 
     number_states=n_states
+    #States and rewards are
     states=np.arange(1,number_states+1,1)
     rewards=np.random.randint(low=0,high=5,size=len(states))
+    
+    #Dictionary with states as key, rewards as value.
     total_info = dict(zip(states,rewards))
+    
+    #Learner dictionary with index for key.
     learned_info={}
     for i in range(n_learners):
         learned_info[i]={}
+        
+        #Randomly generated number of states visited with random number and then generated)
         number_states_visited=np.random.randint(low=2,high=n_states)
+        
+        #Array of visited state numbers
         visited_states=random.choices(states,k=number_states_visited)
+        
+        #Edges exist from the known states to the next one?
         edges_exist = [(i,i+1) for i in range(len(visited_states)-1)]
+        
+        #All possible edges between visited states.
         total_edges = [(i,j) for i in visited_states for j in visited_states]
         edges_unknown = []
+        
         for edge in total_edges:
+            #Ignore if i and j are the same.
             if edge[0]==edge[1]:
                 pass
+            #Append to edges unknown if not known to exist.
             elif edge not in edges_exist:
                 edges_unknown.append(edge)
+        
+        #Generate some non-existing edges?
         number_of_non_existing = np.random.randint(low=1,high=len(edges_unknown))
+        
+        #Select some random edges unknown.
         edges_unknown = random.choices(edges_unknown,k=number_of_non_existing)
+        
+        #Get rewards for states visited.
         rewards_obtained=[total_info[i] for i in visited_states]
+        
+        #Visited state reward knowledge
         learned_info[i]["state-reward"]=dict(zip(visited_states,rewards_obtained))
+        
+        #Edges unknown
         learned_info[i]["not_known"]=edges_unknown
+        
+        #Return sets of state-reward and not-known
     return learned_info
 
 if __name__== "__main__":
 
-    
+    #Generate a random number of states and learners
     n_states=np.random.randint(low=20,high=25)
     n_learners=np.random.randint(low=2,high=n_states//3)
+    
+    #Create assumed learned info (Unecessary once replaced.
     learned_info=create_sols(n_states,n_learners)
+    print("Learned info")
+    for learner in learned_info:
+        print(learner)
+    
+    #Start timer
     now = datetime.now().time()
     print(now)
+    
     for learner in learned_info:
         print("value found by learner "+str(learner), sum(list(learned_info[learner]['state-reward'].values())))
     
+    #Apply the crossover to the dictionary of agents.
+    print("Learned info")
+    print(learned_info[0])
     crossover=smart_crossover.Smart_Crossover(learned_info)
+    
+    #End timer
     after = datetime.now().time()
+    
     print("time without plots")
     print("number of states: ",n_states)
     print("n_iter:",crossover.n_iter)
--- smart_crossover.py
+++ smart_crossover.py
@@ -1,3 +1,6 @@
+#Sam, modified by Kyle Norland
+#Modified 10/10/22
+
 import numpy as np
 import cvxpy as cvx
 import random
@@ -13,10 +16,20 @@ class Smart_Crossover:
     def __init__(self,learners_info):
         
         self.learned_info=learners_info
+        
+        #Generate the ensemble.
         self.ensemble = self.ensemble_learned_info()
+        
+        print("Ensembled learned info")
+        #NOTE: Ensemble unlearned will blow up.
+        print(self.ensemble)
+        #Graph it
         self.graph = nx.DiGraph()
         
+        #Prepare
         rew_matrix,source_state,sink_state = self.prep()
+        
+        
         self.solution=[]
         condition=True
         cycle = []
@@ -31,20 +44,28 @@ class Smart_Crossover:
         
         
     def ensemble_learned_info(self):
-
+        #Collect globally known data.
         total_info={}
         edges={}
         unknown_exist=[]
         for learner in self.learned_info:
+            #Edges has visited states?
             edges[learner]=list(self.learned_info[learner]["state-reward"])
+            print("Edges for learner")
+            print(edges[learner])
+            
             for state in self.learned_info[learner]["state-reward"]:
                 if state not in list(total_info):
                     total_info[state]=self.learned_info[learner]["state-reward"][state]
+            #Is there a check for if another state has learned connections that are not known to another?        
             for unknowns in self.learned_info[learner]["not_known"]:
                 if unknowns not in unknown_exist:
                     unknown_exist.append(unknowns)
+                    
         total_info={"state-reward":total_info,"edges":edges,"not_known":unknown_exist}
         return total_info
+        
+        
     def color_nodes(self,g):
         nodes_with_reward=[]
         nodes_without_reward=[]
@@ -70,6 +91,8 @@ class Smart_Crossover:
             else:
                 edges_color.append('black')
         return edges_color
+        
+        
     def prep(self):
 
         sns.set(style='darkgrid')
@@ -81,7 +104,12 @@ class Smart_Crossover:
                 new.append(j)
         
         array_states=[*set(new)]
+        
+        #Randomly generated end of edges?
         array_edges=[(i[j],i[j+1]) for i in dict_states for j in range(len(i)-1)]
+        
+        print("Array edges")
+        print(array_edges)
         self.graph.add_nodes_from(array_states)
         self.graph.add_edges_from(array_edges)
         
@@ -102,6 +130,7 @@ class Smart_Crossover:
         choose_learner = random.choice(list(self.learned_info))
         source_state = list(self.learned_info[choose_learner]["state-reward"])[0]      
         #randomize sink
+        #!!! Will sometimes cause infinite loop
         sink_state = source_state
         while sink_state == source_state:
             choose_learner = random.choice(list(self.learned_info))
@@ -110,6 +139,7 @@ class Smart_Crossover:
         #color[list(self.graph.nodes).index(source_state)]="green"
         #color[list(self.graph.nodes).index(sink_state)]="brown"
         
+        #Make the adjacency matrix out of the previously added edges.
         adj_matrix = np.array(nx.attr_matrix(self.graph,rc_order=self.graph.nodes))
         rew_matrix = np.zeros(adj_matrix.shape)
         for row_index in range(len(adj_matrix)):
@@ -119,7 +149,8 @@ class Smart_Crossover:
                 
 
         self.graph.add_edges_from(self.ensemble['not_known'])
-
+        
+        #If altered, make it -2 reward for nonexistent edges?
         altered = np.array(nx.attr_matrix(self.graph,rc_order=self.graph.nodes))
         for row_index in range(len(altered)):
             for col_index in range(len(altered)): 
@@ -136,12 +167,15 @@ class Smart_Crossover:
 
     def optimize(self,rew_matrix,source_state,sink_state,cycle):
         
+        #Extra computational time from making multiple adjacency matrices?
         adj_matrix = np.array(nx.attr_matrix(self.graph,rc_order=self.graph.nodes))
+        
         #create variables
         x=cvx.Variable(adj_matrix.shape)
         #create constraints, x is binary
         constraints = [x<=1,x>=0]
         
+        #Could this be set to <= 0 for better performance?
         for row_index in range(len(adj_matrix)):
             for col_index in range(len(adj_matrix)): 
                 if adj_matrix[row_index][col_index]==0:
@@ -149,6 +183,8 @@ class Smart_Crossover:
                     constraints += [x[row_index][col_index]==0]
                 
                 #constraint of only allowing directed edges
+                #!!!!!!!!!!!!!
+                #TODO: Fix, this one doesn't make sense.
                 constraints += [x[row_index][col_index]+x[col_index][row_index]<=1]
        
 
@@ -163,6 +199,8 @@ class Smart_Crossover:
         constraints += [cvx.sum(x,axis=1)<=1]
 
         #Add constraints for found cycles:
+        #Q: Can cycles be avoided with some constraints?
+        
         for cycle_i in self.cycle: 
             index_nodes_cycle = [list(self.graph.nodes).index(node) for node in cycle_i]
             edges_cycle = [(index_nodes_cycle[i],index_nodes_cycle[i+1]) for i in range(len(index_nodes_cycle)-1)]
@@ -175,6 +213,7 @@ class Smart_Crossover:
 
         #create obj_function 
         problem=cvx.Problem(cvx.Maximize(cvx.sum(cvx.multiply(rew_matrix,x))),constraints)
+        
         #solve
         #for writing model//works when unimodular
         #problem.solve("CPLEX",cplex_filename="model.lp")
@@ -211,19 +250,23 @@ class Smart_Crossover:
                 return list(g.edges),cycles
             
             else: 
-                #nodes = g.nodes
-                #new_colors = []
-                #color = self.color_nodes(g)
-                #color[list(g.nodes).index(list(self.graph.nodes)[source_state])]="green"
-                #color[list(g.nodes).index(list(self.graph.nodes)[sink_state])]="brown"
-                #edges_color = self.color_edges(g)
-                #for node in nodes:
-                #    new_list=(list(self.graph.nodes))
-                #    new_colors.append(color[new_list.index(node)])
-#
-                ##plt.figure(figsize=(20,10))
-                #nx.draw(g,node_color=new_colors,edge_color=edges_color,with_labels=True)
-                #plt.savefig("rebuild_graph.png")    
+                '''
+                nodes = g.nodes
+                new_colors = []
+                color = self.color_nodes(g)
+                
+                color[list(g.nodes).index(list(self.graph.nodes)[source_state])]="green"
+                color[list(g.nodes).index(list(self.graph.nodes)[sink_state])]="brown"
+                edges_color = self.color_edges(g)
+                for node in nodes:
+                    new_list=(list(self.graph.nodes))
+                    new_colors.append(color[new_list.index(node)])
+
+                plt.figure(figsize=(20,10))
+                nx.draw(g,node_color=new_colors,edge_color=edges_color,with_labels=True)
+                
+                plt.savefig("rebuild_graph.png")
+                '''
                 return list(g.edges),cycles
 
 
